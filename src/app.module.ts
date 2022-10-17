47e72055f2b09a8b05cfe8bbc6a2fbad012266e9
Ryoh Kunihiro <11649545+rkunihiro@users.noreply.github.com>
2022-10-18 00:20:22 +0900

    add cache-manager ioredis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { CacheModule, Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+// eslint-disable-next-line import/no-named-as-default
+import redisStore from "cache-manager-ioredis";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -15,7 +17,20 @@ import { LoggerModule } from "./logger/logger.module";
             envFilePath: [".env"],
             load: [config],
         }),
+
         LoggerModule,
+
+        CacheModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            isGlobal: true,
+            useFactory: async (config: ConfigService) => {
+                return {
+                    store: redisStore,
+                    url: config.get<string>("redis.url") as string,
+                };
+            },
+        }),
     ],
     providers: [AppService],
     controllers: [AppController],
--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,15 @@
 export interface Config {
     foo: string;
     bar: string;
+    redis: { url: string };
 }
 
 export function config(): Config {
     return {
         foo: process.env["FOO"] ?? "",
         bar: process.env["BAR"] ?? "",
+        redis: {
+            url: process.env["REDIS_URL"] ?? "",
+        },
     };
 }
