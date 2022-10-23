700f275d9be4e5a6f972e45c1160101dbdcbcdff
Ryoh Kunihiro <11649545+rkunihiro@users.noreply.github.com>
2022-10-23 20:52:44 +0900

    CacheService example code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,31 @@
 import { Controller, Get, Inject } from "@nestjs/common";
 
 import { AppService } from "./app.service";
+import { CacheService } from "./cache/cache.service";
 
 @Controller("/")
 export class AppController {
     constructor(
         @Inject(AppService)
         private readonly appService: AppService,
+        @Inject(CacheService)
+        private readonly cacheService: CacheService,
     ) {}
 
     @Get("/")
     public async index() {
         return await this.appService.getMessage();
+    }
+
+    @Get("/now")
+    public async now() {
+        const key = "now";
+        const cache = await this.cacheService.get(key);
+        if (cache) {
+            return cache;
+        }
+        const now = new Date().toISOString();
+        this.cacheService.set(key, now, 60);
+        return now;
     }
 }
